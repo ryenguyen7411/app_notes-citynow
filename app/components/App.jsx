8a22f792d5e9ec031f78bf7126dbd2e0ecf56a22
ryenguyen7411 <ryenguyen7411@gmail.com>
2017-02-16 10:22:50 +0700

    - Pushing data to App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,20 +3,49 @@ import uuid from 'uuid';
 
 import Notes from './Notes';
 
-const notes = [
-    {
-        id: uuid.v4(),
-        task: 'Learn React'
-    },
-    {
-        id: uuid.v4(),
-        task: 'Do laundry'
-    }
-];
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-export default () => (
-    <div>
-        <button onClick={() => console.log('add note')}>+</button>
-        <Notes notes={notes} />
-    </div>
-);
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        };
+    }
+    render() {
+        const {notes} = this.state;
+
+        return (
+            <div>
+                <button onClick={this.addNote}>+</button>
+                <Notes notes={notes} />
+            </div>
+        );
+    }
+    addNote = () => {
+        // It would be possible to write this in an imperative style.
+        // I.e., through `this.state.notes.push` and then
+        // `this.setState({notes: this.state.notes})` to commit.
+        //
+        // I tend to favor functional style whenever that makes sense.
+        // Even though it might take more code sometimes, I feel
+        // the benefits (easy to reason about, no side effects)
+        // more than make up for it.
+        //
+        // Libraries, such as Immutable.js, go a notch further.
+        this.setState({
+            notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: 'New task'
+            }])
+        });
+    }
+}
