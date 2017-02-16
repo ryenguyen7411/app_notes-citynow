0b252022625774d787b04bedaa2c411620151cd6
ryenguyen7411 <ryenguyen7411@gmail.com>
2017-02-16 10:15:17 +0700

    - Pushing data to App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-
 import uuid from 'uuid';
+
+import Notes from './Notes';
 
 const notes = [
     {
-        //id: '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a',
-
         id: uuid.v4(),
         task: 'Learn React'
     },
     {
-        //id: '11bbffc8-5891-4b45-b9ea-5c99aadf870f',
-
         id: uuid.v4(),
         task: 'Do laundry'
     }
 ];
 
 export default () => (
-    <ul>{notes.map(note =>
-        <li key={note.id}>{note.task}</li>
-    )}</ul>
-)
+    <div>
+        <button onClick={() => console.log('add note')}>+</button>
+        <Notes notes={notes} />
+    </div>
+);
--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Notes from './components/Notes';
+import App from './components/App';
 
 if (process.env.NODE_ENV !== 'production') {
   React.Perf = require('react-addons-perf');
 }
 
 ReactDOM.render(
-  // <div>Hello Rye</div>,
-
-  <Notes />,
+  <App />,
   document.getElementById('app')
 );
