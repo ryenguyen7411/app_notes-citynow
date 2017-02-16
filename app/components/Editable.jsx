1ea923ee2be1530e87bb4438967ecea9d6d2b1e1
ryenguyen7411 <ryenguyen7411@gmail.com>
2017-02-16 10:58:47 +0700

    - Add feature edit note

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 
-export default ({editing, value, onEdit, ...props}) => {
+const Editable = ({editing, value, onEdit}) => {
     if (editing) {
-        return <Edit value={value} onEdit={onEdit} {...props} />;
+        return <Editable.Edit value={value} onEdit={onEdit} />;
     }
 
-    return <span {...props}>value: {value}</span>;
+    return <Editable.Value value={value} />;
+};
+
+Editable.Value = ({value, ...props}) => <span {...props}>{value}</span>
+
+class Edit extends React.Component {
+    render() {
+        const {value, onEdit, ...props} = this.props;
+
+        return <input
+            type="text"
+            autoFocus={true}
+            defaultValue={value}
+            onBlur={this.finishEdit}
+            onKeyPress={this.checkEnter}
+            {...props} />;
+    }
+    checkEnter = (e) => {
+        if (e.key === 'Enter') {
+            this.finishEdit(e);
+        }
+    }
+    finishEdit = (e) => {
+        const value = e.target.value;
+
+        if (this.props.onEdit) {
+            this.props.onEdit(value);
+        }
+    }
 }
 
-const Edit = ({onEdit = () => { }, value, ...props}) => (
-    <div onClick={onEdit} {...props}>
-        <span>edit: {value}</span>
-    </div>
-);
+Editable.Edit = Edit;
+
+export default Editable;
