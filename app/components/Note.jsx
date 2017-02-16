7d90b34f46d3c9fa13dcee9e9657e5c2ad99ea69
ryenguyen7411 <ryenguyen7411@gmail.com>
2017-02-16 14:56:07 +0700

    - Implemented drag and drop feature

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -5,14 +5,16 @@ import { DragSource, DropTarget } from 'react-dnd';
 import ItemTypes from '../constants/itemTypes';
 
 const Note = ({
-    connectDragSource, connectDropTarget,
-
-    onMove, id, children, ...props
+    connectDragSource, connectDropTarget, isDragging,
+    isOver, onMove, id, editing, children, ...props
 }) => {
-    return compose(connectDragSource, connectDropTarget)(
-        <div {...props}>
-            {children}
-        </div>
+    // Pass through if we are editing
+    const dragSource = editing ? a => a : connectDragSource;
+
+    return compose(dragSource, connectDropTarget)(
+        <div style={{
+            opacity: isDragging || isOver ? 0 : 1
+        }} {...props}>{children}</div>
     );
 };
 
@@ -37,10 +39,12 @@ const noteTarget = {
 };
 
 export default compose(
-    DragSource(ItemTypes.NOTE, noteSource, connect => ({
-        connectDragSource: connect.dragSource()
+    DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging()
     })),
-    DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
-        connectDropTarget: connect.dropTarget()
+    DropTarget(ItemTypes.NOTE, noteTarget, (connect, monitor) => ({
+        connectDropTarget: connect.dropTarget(),
+        isOver: monitor.isOver()
     }))
 )(Note)
